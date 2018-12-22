8bf09b36a276f8ff1d1916efbb06f3dd6809e791
Kyle <user@swift3.hollandnet.local>
2018-12-22 12:10:32 -0700

    Imported textures

--- textures.js
+++ textures.js
@@ -1,7 +1,8 @@
 var textureMap;
 
 function initTextures() {
-  textureMap = THREE.ImageUtils.loadTexture("textureMap.png");
+  //TODO: dynamically-generated texture map
+  textureMap = THREE.ImageUtils.loadTexture("textures/textureMap.png");
   textureMap.minFilter = THREE.NearestFilter;
   textureMap.magFilter = THREE.NearestFilter;
 }
