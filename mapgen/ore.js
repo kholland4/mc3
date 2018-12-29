f2479596ff19039b1ce2720bef01c828fa3bbb70
Kyle <user@swift3.hollandnet.local>
2018-12-28 18:50:45 -0700

    Coal, iron, gold, and diamond ore generation

---
+++ mapgen/ore.js
@@ -0,0 +1,43 @@
+function coalOreNoise(position) {
+  if(position.y > 0) {
+    return false;
+  }
+  
+  var scale = 4;
+  var thresh = 0.75;
+  
+  return noise3D(position, scale) > thresh;
+}
+
+function ironOreNoise(position) {
+  if(position.y > -64) {
+    return false;
+  }
+  
+  var scale = 3;
+  var thresh = 0.76;
+  
+  return noise3D(position, scale) > thresh;
+}
+
+function goldOreNoise(position) {
+  if(position.y > -128) {
+    return false;
+  }
+  
+  var scale = 5;
+  var thresh = 0.83;
+  
+  return noise3D(position, scale) > thresh;
+}
+
+function diamondOreNoise(position) {
+  if(position.y > -128) {
+    return false;
+  }
+  
+  var scale = 3.5;
+  var thresh = 0.83;
+  
+  return noise3D(position, scale) > thresh;
+}
