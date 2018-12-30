14656b5ed9ea4f100f2aa1b902207838dc9217d2
Kyle <user@swift3.hollandnet.local>
2018-12-30 11:20:04 -0700

    naturally-generated birch and spruce trees

--- mapgen/tree.js
+++ mapgen/tree.js
@@ -51,9 +51,136 @@ function initTrees() {
          air,    air,    air,    air,    air,
          air,    air,    air,    air,    air
     ]
-  })
+  });
+  
+  wood = getItemID("default:birch_log");
+  leaves = getItemID("default:birch_leaves");
+  
+  treeData.push({
+    size: new THREE.Vector3(5, 7, 5),
+    data: [
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+      leaves, leaves, leaves, leaves, leaves,
+      leaves, leaves, leaves, leaves, leaves,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+         
+         air,   air, leaves,   air,    air,
+         air, leaves, leaves, leaves,    air,
+      leaves, leaves, leaves, leaves, leaves,
+      leaves, leaves, leaves, leaves, leaves,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+         
+         air, leaves, leaves, leaves,    air,
+         air, leaves,   wood, leaves,    air,
+      leaves, leaves,   wood, leaves, leaves,
+      leaves, leaves,   wood, leaves, leaves,
+         air,    air,   wood,    air,    air,
+         air,    air,   wood,    air,    air,
+         air,    air,   wood,    air,    air,
+         
+         air,   air, leaves,   air,    air,
+         air, leaves, leaves, leaves,    air,
+      leaves, leaves, leaves, leaves, leaves,
+      leaves, leaves, leaves, leaves, leaves,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+         
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+      leaves, leaves, leaves, leaves, leaves,
+      leaves, leaves, leaves, leaves, leaves,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air
+    ]
+  });
+  
+  wood = getItemID("default:spruce_log");
+  leaves = getItemID("default:spruce_leaves");
+  
+  treeData.push({
+    size: new THREE.Vector3(7, 8, 7),
+    data: [
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air, leaves, leaves, leaves, leaves, leaves,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air, leaves, leaves, leaves,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air, leaves, leaves, leaves,    air,    air,
+      leaves, leaves, leaves, leaves, leaves, leaves, leaves,
+         air,    air,    air,    air,    air,    air,    air,
+         
+         air,    air,    air, leaves,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air, leaves,    air,    air,    air,
+         air, leaves, leaves, leaves, leaves, leaves,    air,
+         air,    air,    air, leaves,    air,    air,    air,
+         air, leaves, leaves, leaves, leaves, leaves,    air,
+      leaves, leaves, leaves, leaves, leaves, leaves, leaves,
+         air,    air,    air,    air,    air,    air,    air,
+         
+         air,    air, leaves, leaves, leaves,    air,    air,
+         air,    air,    air,   wood,    air,    air,    air,
+         air,    air, leaves,   wood, leaves,    air,    air,
+         air, leaves, leaves,   wood, leaves, leaves,    air,
+         air,    air, leaves,   wood, leaves,    air,    air,
+         air, leaves, leaves,   wood, leaves, leaves,    air,
+      leaves, leaves, leaves,   wood, leaves, leaves, leaves,
+         air,    air,    air,   wood,    air,    air,    air,
+         
+         air,    air,    air, leaves,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air, leaves,    air,    air,    air,
+         air, leaves, leaves, leaves, leaves, leaves,    air,
+         air,    air,    air, leaves,    air,    air,    air,
+         air, leaves, leaves, leaves, leaves, leaves,    air,
+      leaves, leaves, leaves, leaves, leaves, leaves, leaves,
+         air,    air,    air,    air,    air,    air,    air,
+         
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air, leaves, leaves, leaves,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air, leaves, leaves, leaves,    air,    air,
+      leaves, leaves, leaves, leaves, leaves, leaves, leaves,
+      
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air,    air,    air,    air,    air,    air,    air,
+         air, leaves, leaves, leaves, leaves, leaves,    air,
+         air,    air,    air,    air,    air,    air,    air
+    ]
+  });
 }
 
 function genTree(pos) {
+  var noise = treeNoise(new THREE.Vector2(pos.x, pos.z));
+  if(noise < 0.88) {
+    return treeData[0];
+  } else if(noise < 0.9) {
+    return treeData[1];
+  } else if(noise < 1.0) {
+    return treeData[2];
+  }
   return treeData[0];
 }
